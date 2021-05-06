16ff3244af0b96ec04facb6257f611f3ccfcd826
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-06 14:30:45 -0500

    updated Customer and Employee model

--- trash_collector_proj/customers/models.py
+++ trash_collector_proj/customers/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 # Create your models here.
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
@@ -6,4 +8,14 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
+    weekly_pickup = models.CharField(max_length=50)
+    one_time_pickup = models.DateField()
+    balance = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    zipcode = models.CharField(max_length=50)
+    #  password = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
