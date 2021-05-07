14f0349ff51fbc3a74f331987efcdb4611c1ffd0
Nessiah919 <nesmith.charlesii@gmail.com>
2021-05-07 12:09:26 -0400

    created a 'create' method in views.py of customer app

--- trash_collector_proj/customers/views.py
+++ trash_collector_proj/customers/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from .models import Customer
 # Create your views here.
@@ -17,4 +17,17 @@ def index(request):
 
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        weekly_pickup = request.POST.get('weekly_pickup')
+        one_time_pickup = request.POST.get('one_time_pickup')
+        balance = request.POST.get('balance')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        address = request.POST.get('address')
+        zip_code = request.POST.get('zip_code')
+        new_customer = Customer(name=name, weekly_pickup=weekly_pickup, one_time_pickup=one_time_pickup, balance=balance, start_date=start_date, end_date=end_date, address=address, zip_code=zip_code)
+        new_customer.save()
+        return HttpResponseRedirect(reversed('customers:new_customer'))
+    else:
+        return render(request, '/home.html')
